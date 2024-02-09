d183b5ae7edb03007084240fed929ddedea90c67
Arul Mathur <arulster17@gmail.com>
2024-02-08 17:21:47 -0800

    weeeweweweweweweowewoeoweowowoweoe

--- TestDocSearch.java
+++ TestDocSearch.java
@@ -8,16 +8,24 @@ import java.io.IOException;
 public class TestDocSearch {
 	@Test 
 	public void testIndex() throws URISyntaxException, IOException {
-    Handler h = new Handler("./technical/");
-    URI rootPath = new URI("http://localhost/");
+    		Handler h = new Handler("./technical/");
+    		URI rootPath = new URI("http://localhost/");
     assertEquals("There are 1391 total files to search.", h.handleRequest(rootPath));
 	}
 	@Test 
 	public void testSearch() throws URISyntaxException, IOException {
-    Handler h = new Handler("./technical/");
-    String sep = File.separator;
-    URI rootPath = new URI("http://localhost/search?q=Resonance");
-    String expect = String.format("Found 2 paths:\n.%stechnical%sbiomed%sar615.txt\n.%stechnical%splos%sjournal.pbio.0020150.txt", sep, sep, sep, sep, sep, sep);
+    	Handler h = new Handler("./technical/");
+    	String sep = File.separator;
+    	URI rootPath = new URI("http://localhost/search?q=Resonance");
+    	String expect = String.format("Found 2 paths:\n.%stechnical%sbiomed%sar615.txt\n.%stechnical%splos%sjournal.pbio.0020150.txt", sep, sep, sep, sep, sep, sep);
+    assertEquals(expect, h.handleRequest(rootPath));
+	}
+	@Test 
+	public void testSearch2() throws URISyntaxException, IOException {
+    	Handler h = new Handler("./technical/");
+    	String sep = File.separator;
+    	URI rootPath = new URI("http://localhost/search?q=Darwin");
+    	String expect = String.format("Found 8 paths:\n.%stechnical%sbiomed%s1471-2105-3-2.txt\n.%stechnical%splos%sjournal.pbio.0020046.txt\n.%stechnical%splos%sjournal.pbio.0020071.txt\n.%stechnical%splos%sjournal.pbio.0020302.txt\n.%stechnical%splos%sjournal.pbio.0020311.txt\n.%stechnical%splos%sjournal.pbio.0020346.txt\n.%stechnical%splos%sjournal.pbio.0020347.txt\n.%stechnical%splos%sjournal.pbio.0020439.txt", sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep, sep);
     assertEquals(expect, h.handleRequest(rootPath));
 	}
 }
